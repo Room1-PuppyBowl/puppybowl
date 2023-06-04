94bb61f798f74b6eadb3732d136c5a21848b9b28
hector <hpulido1021@gmail.com>
2023-06-04 15:48:30 -0400

    Feature: Created fetchSinglePlayer function

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const playerContainer = document.getElementById('all-players-container');
 const newPlayerFormContainer = document.getElementById('new-player-form');
 
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
-const cohortName = 'YOUR COHORT NAME HERE';
+const cohortName = '2302-ACC-CTWEB-PT-B';
 // Use the APIURL variable for fetch requests
-const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
+const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/players`;
 
 /**
  * It fetches all players from the API and returns them
@@ -12,7 +12,10 @@ const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
  */
 const fetchAllPlayers = async () => {
     try {
-
+        const response = await fetch(APIURL);
+        const players = await response.json();
+        console.log(players)
+        return players;
     } catch (err) {
         console.error('Uh oh, trouble fetching players!', err);
     }
@@ -20,7 +23,10 @@ const fetchAllPlayers = async () => {
 
 const fetchSinglePlayer = async (playerId) => {
     try {
-
+        const response = await fetch(`${APIURL}/${playerId}`);
+        const player = await response.json();
+        console.log(player)
+        return player;
     } catch (err) {
         console.error(`Oh no, trouble fetching player #${playerId}!`, err);
     }
@@ -72,8 +78,46 @@ const renderAllPlayers = (playerList) => {
         console.error('Uh oh, trouble rendering players!', err);
     }
 };
+// const renderSinglePlayerById = async (id) => {
+//     try {
+//     const player = await fetchSinglePlayer(id);
+//     console.log(player)
+//     const playerDetailsElement = document.createElement('div');
+//     playerDetailsElement.classList.add('player-details');
+//     playerDetailsElement.innerHTML = `
+//             <h2>${player.name}</h2>
+//             <p>${player.breed}</p>
+//             <p>${player.status}</p>
+//             <img>${player.imageUrl}</img>
+//             <h3>team:</h3>
+//             <ul>
+//             ${team
+//               .map(
+//                 (team, index) => `
+//               <li>
+//                 <div>${team.name}</div>
+                
+//               </li>
+//             `
+//               )
+//               .join('')}
+//           </ul>
+          
 
 
+//             <button class="close-button">Close</button>
+//         `;
+//     playerContainer.appendChild(playerDetailsElement);
+
+//     // add event listener to close button
+//     const closeButton = playerDetailsElement.querySelector('.close-button');
+//     closeButton.addEventListener('click', () => {
+//       playerDetailsElement.remove();
+//     });
+//     } catch (error) {
+//     console.error(error);
+//   }
+// }
 /**
  * It renders a form to the DOM, and when the form is submitted, it adds a new player to the database,
  * fetches all players from the database, and renders them to the DOM.
@@ -89,7 +133,6 @@ const renderNewPlayerForm = () => {
 const init = async () => {
     const players = await fetchAllPlayers();
     renderAllPlayers(players);
-
     renderNewPlayerForm();
 }
 

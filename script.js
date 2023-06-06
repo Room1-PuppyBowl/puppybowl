5c562b44dab734203f32f4bf4fb7bffb364307b0
DebFullstack <debbieking1979@gmail.com>
2023-06-05 20:02:33 -0400

    added add player form in js

--- script.js
+++ script.js
@@ -26,7 +26,82 @@ const fetchSinglePlayer = async (playerId) => {
     }
 };
 
+
+function createForm() {
+  const formContainer = document.getElementById("formContainer");
+
+  // Create the form element
+  const form = document.createElement("form");
+  form.id = "myForm";
+
+  // Create the form fields
+  const nameLabel = document.createElement("label");
+  nameLabel.textContent = "Name:";
+  const nameInput = document.createElement("input");
+  nameInput.type = "text";
+  nameInput.name = "name";
+  nameInput.required = true;
+
+const breedLabel = document.createElement("label");
+  breedLabel.textContent = "Name:";
+  const breedInput = document.createElement("input");
+  breedInput.type = "text";
+  breedInput.breed = "breed";
+  breedInput.required = true;
+
+  const imageLabel = document.createElement("label");
+  imageLabel.textContent = "Image URL:";
+  const imageInput = document.createElement("input");
+  imageInput.type = "text";
+  imageInput.name = "image";
+
+
+  const submitButton = document.createElement("input");
+  submitButton.type = "submit";
+  submitButton.value = "Submit";
+  submitButton.innerHTML = "Submit";
+
+  // Append the form fields to the form
+  form.appendChild(nameLabel);
+  form.appendChild(nameInput);
+  form.appendChild(breedLabel);
+  form.appendChild(breedInput);
+  form.appendChild(statusLabel);
+  form.appendChild(statusInput);
+  form.appendChild(imageLabel);
+  form.appendChild(imageInput);
+  form.appendChild(submitButton);
+
+  // Append the form to the form container
+  formContainer.appendChild(form);
+
+  // Add form submission event listener
+  form.addEventListener("submit", (event) => {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Retrieve the entered values
+    const name = nameInput.value;
+    const email = emailInput.value;
+
+    // Perform any necessary form validation
+
+    // Process the entered data
+    // You can send the data to a server, perform some calculations, or display it on the page
+
+    // Optionally, reset the form after submission
+    form.reset();
+
+    // Remove the form from the form container
+    formContainer.removeChild(form);
+  });
+}
+
+
 const addNewPlayer = async (playerObj) => {
+    const playerDetailsElement = document.createElement("div");
+    playersDetailsElement.innerHTML = `
+    <
+    `
     try {
 
     } catch (err) {
